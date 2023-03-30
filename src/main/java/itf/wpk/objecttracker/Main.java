1911b511bb6d8c159591f0bdb03e81714acbb708
Zwnow <svenotimm@gmail.com>
2023-03-30 09:10:48 +0200

    Added Tutorial Code

--- src/main/java/itf/wpk/objecttracker/Main.java
+++ src/main/java/itf/wpk/objecttracker/Main.java
@@ -1,10 +1,44 @@
 package itf.wpk.objecttracker;
 
 import nu.pattern.OpenCV;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         OpenCV.loadShared();
+        Mat loadedImage = loadImage(Main.class.getResource("/TestImg.jpg").toString());
+        MatOfRect facesDetected = new MatOfRect();
+
+        //Cascade Classifier
+        CascadeClassifier cascadeClassifier = new CascadeClassifier();
+        int minFaceSize = Math.round(loadedImage.rows() * 0.1f);
+        cascadeClassifier.load(Main.class.getResource("haarcascade_frontalface_default.xml").toString());
+        cascadeClassifier.detectMultiScale(loadedImage,
+                facesDetected,
+                1.2,
+                3,
+                Objdetect.CASCADE_SCALE_IMAGE,
+                new Size(minFaceSize, minFaceSize),
+                new Size()
+        );
+
+        Rect[] facesArray = facesDetected.toArray();
+        for(Rect face : facesArray) {
+            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new
+                    Scalar(0,0,255), 3);
+        }
+        saveImage(loadedImage, "C:\\Users");
+    }
+
+    public static Mat loadImage(String imagePath) {
+        return Imgcodecs.imread(imagePath);
+    }
+
+    public static void saveImage(Mat imageMatrix, String targetPath) {
+        Imgcodecs.imwrite(targetPath, imageMatrix);
     }
 }
