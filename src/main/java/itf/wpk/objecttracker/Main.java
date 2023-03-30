48d3b6dd7b633f2db4dc22aea1bd74d7c4fa226d
Zwnow <svenotimm@gmail.com>
2023-03-30 08:40:55 +0200

    Added Shadow Plugin and loaded OpenCV lib

--- src/main/java/itf/wpk/objecttracker/Main.java
+++ src/main/java/itf/wpk/objecttracker/Main.java
@@ -1,7 +1,10 @@
 package itf.wpk.objecttracker;
 
+import nu.pattern.OpenCV;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        OpenCV.loadShared();
     }
 }
